5eb2f0f5db6eaca57da459df9ea077b3f65d7b69
Rekdesnek <ziggy.weesing@gmail.com>
2023-01-10 11:49:17 +0100

    uit elkaar getrokken

--- grid.py
+++ grid.py
@@ -1,3 +1,5 @@
+from pathfinder import Pathfinder
+
 gatesdict = {}
 gatesfilepath = "gates_and_netlists/print_0.csv"
 with open(gatesfilepath) as gatesfile:
@@ -34,64 +36,6 @@ class Grid:
     def __repr__(self):
         return "\n".join([" ".join(row) for row in self.board])
 
-
-class Pathfinder:
-    def __init__(self, start_gate_x, start_gate_y, end_gate_x, end_gate_y, board):
-        self.start_gate_x = start_gate_x
-        self.start_gate_y = start_gate_y
-        self.end_gate_x = end_gate_x
-        self.end_gate_y = end_gate_y
-        self.board = board
-
-    def find(self):
-        route = [(self.start_gate_x, self.start_gate_y)]
-        wire_count = 0
-        current_gate_x = self.start_gate_x
-        current_gate_y = self.start_gate_y
-        end_gate = [(self.end_gate_x, self.end_gate_y)]
-
-        # For now only checking first one direction then other. Not switching when stuck
-        # Fix End gate needs if statement to return to a X
-        while current_gate_x < self.end_gate_x:
-            current_gate_x += 1
-            current_position = (current_gate_x, current_gate_y)
-            route.append(current_position)
-            wire_count += 1
-            self.board[current_gate_x][current_gate_y] = "1"
-            if current_position == end_gate:
-                self.board[current_gate_x][current_gate_y] = "X"
-
-        while current_gate_x > self.end_gate_x:
-            current_gate_x += 1
-            current_position = (current_gate_x, current_gate_y)
-            route.append(current_position)
-            wire_count += 1
-            self.board[current_gate_x][current_gate_y] = "1"
-            if current_position == end_gate:
-                self.board[current_gate_x][current_gate_y] = "X"
-
-        while current_gate_y < self.end_gate_y:
-            current_gate_y += 1
-            current_position = (current_gate_x, current_gate_y)
-            route.append(current_position)
-            wire_count += 1
-            self.board[current_gate_x][current_gate_y] = "1"
-            if current_position == end_gate:
-                self.board[current_gate_x][current_gate_y] = "X"
-
-        while current_gate_y > self.end_gate_y:
-            current_gate_y += 1
-            current_position = (current_gate_x, current_gate_y)
-            route.append(current_position)
-            wire_count += 1
-            self.board[current_gate_x][current_gate_y] = "1"
-            if current_position == end_gate:
-                self.board[current_gate_x][current_gate_y] = "X"
-
-        return route, wire_count, self.board
-
-    def __repr__(self):
-        return "\n".join([" ".join(row) for row in self.board])
 
 # Make a dictionary at the end were net list(route) is linked to the actual found route.
 
