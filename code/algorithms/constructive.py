815b6bc8f8a576d1c1a4fdab5cc0c8c8cae2a8c6
ThomasGlansdorp <thomas.glansdorp@gmail.com>
2023-01-26 12:44:24 +0100

    started on find function with BLS search and readded mainconstructive.py

--- code/algorithms/constructive.py
+++ code/algorithms/constructive.py
@@ -1,8 +1,10 @@
 from ..classes.pathfinder import Pathfinder
-import random
+from code.classes.looptester import looptest
+
 
 class Constructive(Pathfinder):
     """ Subclass for the pathfinder class. This Algoritme determines the shortest path using depth and breth search."""
+
     def __init__(self, start, end, board):
         """
         runs the init of pathfinder, which creates the nessecary class atributes.
@@ -16,4 +18,29 @@ class Constructive(Pathfinder):
         super().__init__(start, end, board)
 
     def find(self):
-        #implementations of depth and or breth search for determining route. Implement 3D part.
+        start = [(self.start_gate_x, self.start_gate_y)]
+        explored = []
+        queue = [[start]]
+        # implementations of depth and or breth search for determining route. Implement 3D part.
+        while queue:
+            route = queue.pop(0)
+            node = route[-1]
+            print(node)
+            print(22)
+
+            if node not in explored:
+                neighbours = self.find_neighbours(node)
+                print(neighbours)
+
+                for neighbour in neighbours:
+                    new_route = list(route)
+                    new_route.append(neighbour)
+                    queue.append(new_route)
+
+                    # if neighbour is self.end_point(neighbour_x, neighbour_y):
+                    #     return new_route
+
+                explored.append(node)
+        return route
+
+
