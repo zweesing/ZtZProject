0d01ec65988b8afe1fd6ed4b8be269f67c03bc54
zoedavidson08 <zoedavidson1970@gmail.com>
2023-01-27 14:25:44 +0100

    greedy maakt oplossingen

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -40,198 +40,197 @@ class Pathfind(Pathfinder):
         random_direction = ["x", "y"]
         random_direction2 = [1, -1]
 
-        # start with x coordinate
-        while current_gate_x < self.end_gate_x:
-            current_gate_x += 1
-
-            # if the new current position has a z coordinate, it should be updated in the route list
-
-            current_position = (current_gate_x, current_gate_y)
-
-            
-            # if the wire has found the end gate, the loop breaks
-            if self.end_point(current_gate_x, current_gate_y, current_gate_z):
-                route.append(current_position)
-                wire_count += 1
-                break
-
-            # if the wire goes the wrong way it should choose the next direction randomly
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                current_gate_x -= 1
-
-                current_gate_x_or_y = random.choice(random_direction)
-                left_or_right = random.choice(random_direction2)
-
-                if current_gate_x_or_y == "x":
-                    current_gate_x += left_or_right
-                else:
-                    current_gate_y += left_or_right
-
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                if current_gate_x_or_y == "x":
-                    # maak de tweede ook random ipv altijd van foute x richting gelijk naar de andere richting overstappen
-                    current_gate_x += -1 * left_or_right
-                    current_gate_y += left_or_right
-                else:
-                    current_gate_y += -1 * left_or_right
-                    current_gate_x += left_or_right
-
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                return "crashed"
-            else:
-                current_position = (current_gate_x, current_gate_y)
-
-            # append the new current position and update the wire count
-            route.append(current_position)
-            wire_count += 1
-
-            # vraag hoe dit moet met een 3d array
-            self.board[current_gate_z][current_gate_y][current_gate_x] = "1"
-
-        # second x direction
-        while current_gate_x > self.end_gate_x:
-            current_gate_x -= 1
-
-            # if the new current position has a z coordinate, it should be updated in the route list
-
-            current_position = (current_gate_x, current_gate_y)
-            
-
-            # if the wire has found the end gate, the loop breaks
-            if self.end_point(current_gate_x, current_gate_y, current_gate_z):
-                route.append(current_position)
-                wire_count += 1
-                break
-
-            # if the wire goes the wrong way it should choose the next direction randomly
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+        while True:
+            # start with x coordinate
+            while current_gate_x < self.end_gate_x:
                 current_gate_x += 1
 
-                current_gate_x_or_y = random.choice(random_direction)
-                left_or_right = random.choice(random_direction2)
+                # if the new current position has a z coordinate, it should be updated in the route list
 
-                if current_gate_x_or_y == "x":
-                    current_gate_x += left_or_right
-                else:
-                    current_gate_y += left_or_right
-
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                if current_gate_x_or_y == "x":
-                    current_gate_x += -1 * left_or_right
-                    current_gate_y += left_or_right
-                else:
-                    current_gate_y += -1 * left_or_right
-                    current_gate_x += left_or_right
-
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                print("crashed")
-                return "crashed"
-            else:
                 current_position = (current_gate_x, current_gate_y)
 
-            # append the new current position and update the wire count
-            route.append(current_position)
-            wire_count += 1
+                # if the wire has found the end gate, the loop breaks
+                if self.end_point(current_gate_x, current_gate_y, current_gate_z):
+                    route.append(current_position)
+                    wire_count += 1
+                    return route, wire_count, self.board
 
-            # vraag hoe dit moet met een 3d array
-            self.board[current_gate_z][current_gate_y][current_gate_x] = "1"
+                # if the wire goes the wrong way it should choose the next direction randomly
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    current_gate_x -= 1
 
-        # y direction
-        while current_gate_y < self.end_gate_y:
-            current_gate_y += 1
+                    current_gate_x_or_y = random.choice(random_direction)
+                    left_or_right = random.choice(random_direction2)
 
-            # if the new current position has a z coordinate, it should be updated in the route list
+                    if current_gate_x_or_y == "x":
+                        current_gate_x += left_or_right
+                    else:
+                        current_gate_y += left_or_right
 
-            current_position = (current_gate_x, current_gate_y)
-            
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    if current_gate_x_or_y == "x":
+                        # maak de tweede ook random ipv altijd van foute x richting gelijk naar de andere richting overstappen
+                        current_gate_x += -1 * left_or_right
+                        current_gate_y += left_or_right
+                    else:
+                        current_gate_y += -1 * left_or_right
+                        current_gate_x += left_or_right
 
-            # if the wire has found the end gate, the loop breaks
-            if self.end_point(current_gate_x, current_gate_y, current_gate_z):
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    return "crashed"
+                else:
+                    current_position = (current_gate_x, current_gate_y)
+
+                # append the new current position and update the wire count
                 route.append(current_position)
                 wire_count += 1
-                break
 
-            # if the wire goes the wrong way it should choose the next direction randomly
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                current_gate_y -= 1
+                # vraag hoe dit moet met een 3d array
+                self.board[current_gate_z][current_gate_y][current_gate_x] = "1"
 
-                current_gate_x_or_y = random.choice(random_direction)
-                left_or_right = random.choice(random_direction2)
+            # second x direction
+            while current_gate_x > self.end_gate_x:
+                current_gate_x -= 1
 
-                if current_gate_x_or_y == "x":
-                    current_gate_x += left_or_right
-                else:
-                    current_gate_y += left_or_right
+                # if the new current position has a z coordinate, it should be updated in the route list
 
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                if current_gate_x_or_y == "x":
-                    current_gate_x += -1 * left_or_right
-                    current_gate_y += left_or_right
-                else:
-                    current_gate_y += -1 * left_or_right
-                    current_gate_x += left_or_right
-
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                print("crashed")
-                return "crashed"
-            else:
                 current_position = (current_gate_x, current_gate_y)
+                
 
-            # append the new current position and update the wire count
-            route.append(current_position)
-            wire_count += 1
+                # if the wire has found the end gate, the loop breaks
+                if self.end_point(current_gate_x, current_gate_y, current_gate_z):
+                    route.append(current_position)
+                    wire_count += 1
+                    return route, wire_count, self.board
 
-            # vraag hoe dit moet met een 3d array
-            self.board[current_gate_z][current_gate_y][current_gate_x] = "1"
+                # if the wire goes the wrong way it should choose the next direction randomly
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    current_gate_x += 1
 
-        # second y direction
-        while current_gate_y > self.end_gate_y:
-            current_gate_y -= 1
+                    current_gate_x_or_y = random.choice(random_direction)
+                    left_or_right = random.choice(random_direction2)
 
-            # if the new current position has a z coordinate, it should be updated in the route list
+                    if current_gate_x_or_y == "x":
+                        current_gate_x += left_or_right
+                    else:
+                        current_gate_y += left_or_right
 
-            current_position = (current_gate_x, current_gate_y)
-            
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    if current_gate_x_or_y == "x":
+                        current_gate_x += -1 * left_or_right
+                        current_gate_y += left_or_right
+                    else:
+                        current_gate_y += -1 * left_or_right
+                        current_gate_x += left_or_right
 
-            # if the wire has found the end gate, the loop breaks
-            if self.end_point(current_gate_x, current_gate_y, current_gate_z):
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):   
+                    return "crashed"
+                else:
+                    current_position = (current_gate_x, current_gate_y)
+
+                # append the new current position and update the wire count
                 route.append(current_position)
                 wire_count += 1
-                break
 
-            # if the wire goes the wrong way it should choose the next direction randomly
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                # vraag hoe dit moet met een 3d array
+                self.board[current_gate_z][current_gate_y][current_gate_x] = "1"
+
+            # y direction
+            while current_gate_y < self.end_gate_y:
                 current_gate_y += 1
 
-                current_gate_x_or_y = random.choice(random_direction)
-                left_or_right = random.choice(random_direction2)
+                # if the new current position has a z coordinate, it should be updated in the route list
 
-                if current_gate_x_or_y == "x":
-                    current_gate_x += left_or_right
-                else:
-                    current_gate_y += left_or_right
-
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                if current_gate_x_or_y == "x":
-                    current_gate_x += -1 * left_or_right
-                    current_gate_y += left_or_right
-                else:
-                    current_gate_y += -1 * left_or_right
-                    current_gate_x += left_or_right
-
-            if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
-                print("crashed")
-                return "crashed"
-            else:
                 current_position = (current_gate_x, current_gate_y)
+                
 
-            # append the new current position and update the wire count
-            route.append(current_position)
-            wire_count += 1
+                # if the wire has found the end gate, the loop breaks
+                if self.end_point(current_gate_x, current_gate_y, current_gate_z):
+                    route.append(current_position)
+                    wire_count += 1
+                    return route, wire_count, self.board
 
-            # vraag hoe dit moet met een 3d array
-            self.board[current_gate_z][current_gate_y][current_gate_x] = "1"
+                # if the wire goes the wrong way it should choose the next direction randomly
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    current_gate_y -= 1
 
-    
+                    current_gate_x_or_y = random.choice(random_direction)
+                    left_or_right = random.choice(random_direction2)
 
-        return route, wire_count, self.board
+                    if current_gate_x_or_y == "x":
+                        current_gate_x += left_or_right
+                    else:
+                        current_gate_y += left_or_right
+
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    if current_gate_x_or_y == "x":
+                        current_gate_x += -1 * left_or_right
+                        current_gate_y += left_or_right
+                    else:
+                        current_gate_y += -1 * left_or_right
+                        current_gate_x += left_or_right
+
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    
+                    return "crashed"
+                else:
+                    current_position = (current_gate_x, current_gate_y)
+
+                # append the new current position and update the wire count
+                route.append(current_position)
+                wire_count += 1
+
+                # vraag hoe dit moet met een 3d array
+                self.board[current_gate_z][current_gate_y][current_gate_x] = "1"
+
+            # second y direction
+            while current_gate_y > self.end_gate_y:
+                current_gate_y -= 1
+
+                # if the new current position has a z coordinate, it should be updated in the route list
+
+                current_position = (current_gate_x, current_gate_y)
+                
+
+                # if the wire has found the end gate, the loop breaks
+                if self.end_point(current_gate_x, current_gate_y, current_gate_z):
+                    route.append(current_position)
+                    wire_count += 1
+                    return route, wire_count, self.board
+
+                # if the wire goes the wrong way it should choose the next direction randomly
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    current_gate_y += 1
+
+                    current_gate_x_or_y = random.choice(random_direction)
+                    left_or_right = random.choice(random_direction2)
+
+                    if current_gate_x_or_y == "x":
+                        current_gate_x += left_or_right
+                    else:
+                        current_gate_y += left_or_right
+
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    if current_gate_x_or_y == "x":
+                        current_gate_x += -1 * left_or_right
+                        current_gate_y += left_or_right
+                    else:
+                        current_gate_y += -1 * left_or_right
+                        current_gate_x += left_or_right
+
+                if not self.is_valid(current_gate_x, current_gate_y, current_gate_z):
+                    
+                    return "crashed"
+                else:
+                    current_position = (current_gate_x, current_gate_y)
+
+                # append the new current position and update the wire count
+                route.append(current_position)
+                wire_count += 1
+
+                # vraag hoe dit moet met een 3d array
+                self.board[current_gate_z][current_gate_y][current_gate_x] = "1"
+
+        
+
+        
