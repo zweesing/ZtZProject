9c5c530d38e5a5cce5378f20c29e8034db4a1935
Rekdesnek <ziggy.weesing@gmail.com>
2023-01-27 15:40:50 +0100

    intersection cost calculator now skips gate locations

--- code/classes/helper_funcs.py
+++ code/classes/helper_funcs.py
@@ -1,11 +1,20 @@
-def intersect_count(routes):
+def intersect_count(routes, gates):
+
+    # make the coords 3d
+    gates_list2d = list(gates.values())
+    gates_list = []
+    for gate in gates_list2d:
+        x, y = gate
+        gates_list.append((x, y, 0))
+
+    print(gates_list)
     seen = set()
     intersect_counter = 0
     for route in routes:
         for coord in route:
             if coord not in seen:
                 seen.add(coord)
-            else:
+            elif coord not in gates_list:
                 intersect_counter += 1
 
     return intersect_counter
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from code.algorithms.random import Pathfindrandom
+from code.algorithms.constructive import Constructive
 from code.classes.looptester import looptest
-from code.visualisation.visualisation import visualize
+from code.visualisation.visualisation3D import visualize
 from code.classes.helper_funcs import intersect_count
+from code.classes.grid import Grid
 
 """
 Runs the tests of a chosen algoritme. With the chosen netlist and gate csv files. 
@@ -11,11 +13,11 @@ data in a csv file.
 """
 
 gatesfilepath = "data/chip_0/print_0.csv"
-netlistpath = "data/chip_0/netlist_1.csv"
+netlistpath = "data/chip_0/netlist_3.csv"
 total_counter = 0
 # get 50 results and see how long that took
 
-results = looptest(Pathfindrandom, gatesfilepath, netlistpath)
+results = looptest(Constructive, gatesfilepath, netlistpath)
 
 path, routes, totalwirecount, crash_counter = results
 total_counter += crash_counter
@@ -26,10 +28,12 @@ print(routes)
 print(totalwirecount)
 print(total_counter)
 
+# stupid way of reading gates in, needed for calculation
+gates, _ = Grid.read_gates(1, gatesfilepath)
 
 # calculate intersections
-intersections = intersect_count(routes)
+intersections = intersect_count(routes, gates)
 
 cost = totalwirecount + 300 * intersections
-
-visualize(routes, gatesfilepath)
+print(f"costs: {cost}, intersections: {intersections}")
+visualize("output.csv", gatesfilepath)
