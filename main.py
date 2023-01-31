286e28c936e588ab12860be1d1a3c44def2039ed
zoedavidson08 <zoedavidson1970@gmail.com>
2023-01-31 12:24:14 +0100

    main comments

--- main.py
+++ main.py
@@ -5,30 +5,32 @@ from code.visualisation.visualisation3D import visualize
 from code.classes.helper_funcs import intersect_count, cost_calc, writetofile
 from code.classes.grid import Grid
 import argparse
-import sys
 
 """
-Runs the tests of a chosen algoritme. With the chosen netlist and gate csv files. 
+Runs the tests of a chosen algoritme a chosen amount of times. With the chosen netlist and gate csv files. 
 
-It returns the last board, last route and the total wire count. In looptest an output file is also created showing the 
+It returns the last board, last route and the total wire count. In looptest the output files are also created showing the 
 data in a csv file.
 """
 
+# Create arguments for algorithm, chip, netlist, iteration
 parser = argparse.ArgumentParser()
 parser.add_argument("--algorithm", "-a", help="Choose between greedy and breadth for respectively a greedy/random algoritm or a breadth first algoritm")
 parser.add_argument("--chip", "-c", type=int, default=0, help="Choose a chip in range 0-2")
 parser.add_argument("--netlist", "-n", type=int, default=1, help="Choose a chip in range 1-3")
 parser.add_argument("--iteration", "-i", type=int, help="Choose the amount of solutions you want returned")
-args = parser.parse_args()
 
+# Unpack arguments
+args = parser.parse_args()
 chip = args.chip
 netlist = args.netlist
 algorithm = args.algorithm
 
+# Load correct gates file and correct netlist file
 gatesfilepath = f"data/chip_{chip}/print_{chip}.csv"
 netlistpath = f"data/chip_{chip}/netlist_{3 * chip + netlist}.csv"
 
-
+# Iterate the correct amount of times, during iteration run the chosen algoritm until it finds a solution
 for i in range(args.iteration):
     if algorithm == "greedy":
         results = looptest(Pathfind, gatesfilepath, netlistpath)
@@ -37,18 +39,17 @@ for i in range(args.iteration):
     path, routes, totalwirecount, crash_counter, netlist = results
     writetofile(netlist, routes, totalwirecount, "greedyrandom_chip1_netlist4", i)
 
+print(f"total wire count: {totalwirecount}")
 
-print(path)
-print(routes)
-print(totalwirecount)
-# print(total_counter)
-
-# stupid way of reading gates in, needed for calculation
+# Read gates for intersection calculator
 gates, _ = Grid.read_gates(1, gatesfilepath)
 
-# calculate intersections
+# Calculate the amount of intersections
 intersections = intersect_count(routes, gates)
 
+# Calculate and print the total cost of placing the wires
 cost = cost_calc(routes, gates, totalwirecount)
 print(f"costs: {cost}, intersections: {intersections}")
+
+# Visualize the outputfile
 visualize("output.csv", gatesfilepath)
