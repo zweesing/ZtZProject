c13581ea0e0fb9262a463d784147525772f5c7ef
zoedavidson08 <zoedavidson1970@gmail.com>
2023-01-31 12:15:16 +0100

    main

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from code.classes.looptester import looptest
 from code.visualisation.visualisation3D import visualize
 from code.classes.helper_funcs import intersect_count, cost_calc, writetofile
 from code.classes.grid import Grid
+import argparse
+import sys
 
 """
 Runs the tests of a chosen algoritme. With the chosen netlist and gate csv files. 
@@ -12,21 +14,33 @@ It returns the last board, last route and the total wire count. In looptest an o
 data in a csv file.
 """
 
-chip = 0
-netlist = 1
+parser = argparse.ArgumentParser()
+parser.add_argument("--algorithm", "-a", help="Choose between greedy and breadth for respectively a greedy/random algoritm or a breadth first algoritm")
+parser.add_argument("--chip", "-c", type=int, default=0, help="Choose a chip in range 0-2")
+parser.add_argument("--netlist", "-n", type=int, default=1, help="Choose a chip in range 1-3")
+parser.add_argument("--iteration", "-i", type=int, help="Choose the amount of solutions you want returned")
+args = parser.parse_args()
+
+chip = args.chip
+netlist = args.netlist
+algorithm = args.algorithm
+
 gatesfilepath = f"data/chip_{chip}/print_{chip}.csv"
 netlistpath = f"data/chip_{chip}/netlist_{3 * chip + netlist}.csv"
 
 
-for i in range(100):
-    results = looptest(Pathfind, gatesfilepath, netlistpath)
+for i in range(args.iteration):
+    if algorithm == "greedy":
+        results = looptest(Pathfind, gatesfilepath, netlistpath)
+    if algorithm == "breadth":
+        results = looptest(Constructive, gatesfilepath, netlistpath)
     path, routes, totalwirecount, crash_counter, netlist = results
     writetofile(netlist, routes, totalwirecount, "greedyrandom_chip1_netlist4", i)
 
 
-# print(path)
-# print(routes)
-# print(totalwirecount)
+print(path)
+print(routes)
+print(totalwirecount)
 # print(total_counter)
 
 # stupid way of reading gates in, needed for calculation
